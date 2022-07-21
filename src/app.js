24f366f7596c9769ce2e716917bd70b9d1ff82dd
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-21 17:01:46 -0300

    chore: add essential folders and libs

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// import routes from "./routers/routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+// app.use(routes);
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+    console.log('server running on PORT ' + PORT)
+})

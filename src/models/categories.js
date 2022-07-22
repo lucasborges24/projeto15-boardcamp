0c393b27ca281414122588f711a37b4dee250fb4
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-22 10:14:45 -0300

    feat: create two function to categoryPOST

--- src/models/categories.js
+++ src/models/categories.js
@@ -3,8 +3,23 @@ import { connection } from "./index.js";
 
 // DATABASE
 export const getCategories = async () => {
-  const categories = await connection.query("SELECT * FROM categories");
+  const sql = "SELECT * FROM categories";
+  const categories = await connection.query(sql);
   return categories.rows;
+};
+
+export const categoryAlreadyExist = async (name) => {
+  const sql = `SELECT (name) FROM categories WHERE name = $1`;
+  const { rows: categoryExist } = await connection.query(sql, [name]);
+  if (categoryExist && categoryExist.length !== 0) {
+    return true;
+  }
+  return false;
+};
+
+export const postCategories = async (name) => {
+  const sql = `INSERT INTO categories (name) VALUES ($1)`;
+  await connection.query(sql, [name]);
 };
 
 // SCHEMAS

c853391127cd87b3a41fe896103aa61c465bdbba
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-22 09:08:01 -0300

    feat: create categorie schema

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,6 +1,13 @@
+import joi from "joi";
 import { connection } from "./index.js";
 
+// DATABASE
 export const getCategories = async () => {
-  const categories = await connection.query('SELECT * FROM categories');
+  const categories = await connection.query("SELECT * FROM categories");
   return categories.rows;
 };
+
+// SCHEMAS
+export const categoriesSchema = joi.object({
+  name: joi.string().trim().required(),
+});

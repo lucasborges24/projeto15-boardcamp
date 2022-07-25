aa16b58430ae1fc1579b871ad11d7ce6097efd4b
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-25 08:59:09 -0300

    feat: create middlewares functions to validate POST customer

---
+++ src/middlewares/customersMiddleware.js
@@ -0,0 +1,37 @@
+import { customers } from "../models/index.js";
+import { stripHtml } from "string-strip-html";
+
+export const validateBody = (req, res, next) => {
+  const validationBefore = customers.customersSchema.validate(req.body);
+  if (validationBefore.error) {
+    return res.status(400).send("Some error with JSON body");
+  }
+
+  const customer = {
+    name: stripHtml(validationBefore.value.name).result,
+    phone: validationBefore.value.phone,
+    cpf: validationBefore.value.cpf,
+    birthday: validationBefore.value.birthday,
+  };
+
+  const validationAfter = customers.customersSchema.validate(customer);
+  if (validationAfter.error) {
+    return res.status(400).send("Some error with JSON body envolving HTML tag");
+  }
+
+  res.locals.customer = customer;
+  next();
+  return true;
+};
+
+export const checkCpfAlreadyExist = async (req, res, next) => {
+    const { cpf } = res.locals.customer;
+    try {
+      const cpfExist = await customers.cpfAlreadyExist(cpf);
+      if (cpfExist) return res.sendStatus(409);
+    } catch (error) {
+      res.sendStatus(500);
+    }
+    next();
+    return true;
+  };

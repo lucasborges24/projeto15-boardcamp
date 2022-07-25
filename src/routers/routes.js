331c2a7c286e31e28b209191435aa22f72afa363
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-25 09:00:01 -0300

    feat: create POST /customer route

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,40 +1,47 @@
 import { Router } from "express";
 
 import {
-  categoriesController as cC,
-  gamesController as gC,
+  categoriesController,
+  gamesController,
+  customersController,
 } from "../controllers/index.js";
 
 import {
-  categoriesMiddleware as cM,
-  gamesMiddleware as gM,
+  categoriesMiddleware,
+  gamesMiddleware,
+  customersMiddleware,
 } from "../middlewares/index.js";
 
 const router = Router();
 
 // categories routes
-router.get("/categories", cC.getCategories);
+router.get("/categories", categoriesController.getCategories);
 router.post(
   "/categories",
-  cM.validateBody,
-  cM.checkCategoryAlreadyExist,
-  cC.postCategories
+  categoriesMiddleware.validateBody,
+  categoriesMiddleware.checkCategoryAlreadyExist,
+  categoriesController.postCategories
 );
 
 // games routes
 router.post(
   "/games",
-  gM.validateBody,
-  gM.checkCategoryIdExist,
-  gM.checkNameAlreadyExist,
-  gC.postGame
+  gamesMiddleware.validateBody,
+  gamesMiddleware.checkCategoryIdExist,
+  gamesMiddleware.checkNameAlreadyExist,
+  gamesController.postGame
 );
-router.get("/games", gM.validateQuery, gC.getGames);
+router.get("/games", gamesMiddleware.validateQuery, gamesController.getGames);
 
 // customers routes
-router.post("/customers", () => console.log('post'))
-router.get('/customers', () => console.log('get'))
-router.get('/customers/:id', () => console.log('get id'))
-router.put('/customers', () => console.log('put'))
+router.post(
+  "/customers",
+  customersMiddleware.validateBody,
+  customersMiddleware.checkCpfAlreadyExist,
+  customersController.postCustomer
+);
+router.get("/customers", () => console.log("get"));
+router.get("/customers/:id", () => console.log("get id"));
+router.put("/customers", () => console.log("put"));
 
 export default router;

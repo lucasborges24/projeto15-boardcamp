4127ff71bbc3d0f76f20ac7a485aaf46862fcd41
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-07-25 13:04:59 -0300

    feat: add body Schema

---
+++ src/models/rentals.js
@@ -0,0 +1,12 @@
+import joi from "joi";
+import { connection } from "./index.js";
+
+// DATABASE
+
+// SCHEMAS
+
+export const bodySchema = joi.object({
+  customerId: joi.number().integer().min(0).required(),
+  gameId: joi.number().integer().min(0).required(),
+  daysRented: joi.number().integer().min(0).required(),
+});
